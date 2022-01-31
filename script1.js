84480df3a56972809b5e640a76429159eeb1fb50
crimegod <dpk.exe@gmail.com>
2022-01-31 18:38:14 +0530

    clock update

--- script1.js
+++ script1.js
@@ -1,21 +1,26 @@
+//takes hour time min variables
 
 var timeHour = null;
 var timeMin = null;
 var timeSeconds  = null;
 
+//creating array
+var saveTime = [];
+
+//Clock ticks Per Secound Function
 function perSec(){
 const date = new Date();
 
-
-
+ //appending 0 to Hour, Minute and Second By Creating AddZero Function (Check Line - 29)
     timeHour = document.getElementById("hour").innerHTML =  addZero(date.getHours());
     timeMin= document.getElementById("min").innerHTML =   addZero(date.getMinutes());
     timeSeconds = document.getElementById("sec").innerHTML =   addZero(date.getSeconds());
 
+    
     if(saveTime.length > 0){
-        let  now = timeHour + timeMin + timeSeconds ;
+        let  now = timeHour + ":" + timeMin + ":" + timeSeconds ;
           for(let i=0; i<saveTime.length; i++){
-          
+          //Playing Sound When current Time And Set Time Become Equal
               if(saveTime[i] == now && sound.paused){
                   sound.play();
               }
@@ -23,7 +28,10 @@ const date = new Date();
           
       }
 }
-    
+setInterval(perSec,1000);
+
+
+//Add Zero Function
 function addZero(num){
    if(num<10){
        num = "0" + num;
@@ -33,11 +41,8 @@ function addZero(num){
     return num;
 }
 
-setInterval(perSec,1000);
 
-
-
-//create  function to add options
+//create  function to add Select And Options
 //create Select
 //create options
 function createSel(max){
@@ -54,11 +59,12 @@ function createSel(max){
       return selector;
 }
 
-let setHour = createSel(23);
-let setMin = createSel(59);
-let setSec = createSel(59);
+let setHour = createSel(23); // creating 0-23  options for hour
+let setMin = createSel(59);  //creating 0-59  options for Minute
+let setSec = createSel(59);  //creating 0-59 options  for second
 
- document.getElementById("shr").appendChild(setHour);
+ //now appending all child of select and options to div set alarm 
+ document.getElementById("shr").appendChild(setHour); 
  document.getElementById("smin").appendChild(setMin);
  document.getElementById("ssec").appendChild(setSec);
 
@@ -68,12 +74,8 @@ let setSec = createSel(59);
 
 var sound = new Audio("wake-up-sound.mp3");
 
-var saveTime = [];
 
-
-
-
-// set and reset
+// creating Set function for creating alarm
 let alarm=null;
 function set(){
      alarm = setHour.value + setMin.value + setSec.value; //string cocatination
@@ -85,61 +87,62 @@ function set(){
     treset.disabled = false;
 
     if (alarm != null) {
-        saveTime.push(alarm);
+        saveTime.push(alarm1);
+        createEle();
       }
-     createEle();
-     k++;
+      //calling create element for creating elements after clicking set
+     
+     
 }
 
 
 
-let k=0;
-let p=0;
 
 
-
+//creating elements for paragraph and buttons 
 function createEle(){
-   
+   let div1 = document.createElement("div");
+   div1.style.width="200px";
+   div1.style.display = "flex";
+   div1.style.justifyContent = "space-between";
+   div1.style.alignItems ="center";
    let p= document.createElement("p");
    let del = document.createElement("button");
+   del.style.height = "20px";
+   del.className = "delButton";
+   div1.appendChild(p);
+   div1.appendChild(del);
   
-    document.getElementById("set-alarm-container").appendChild(p);
-    document.getElementById("set-alarm-container").appendChild(del);
+    document.getElementById("set-alarm-container").appendChild(div1);
     
     del.innerHTML = "DELETE";
     p.innerHTML = alarm1;
-    del.setAttribute("id", "del1");
-    del.setAttribute("value", k);
-    p.setAttribute("id", "time");
-    p.setAttribute("value", k);
-    
-
-    var para = document.getElementById("time");
-    var delete1 = document.getElementById("del1");
-
-    function delEle(){
-       
-    para.parentNode.removeChild(para);
-    delete1.parentNode.removeChild(delete1);
-        
+           
 } 
-let a = document.getElementById("time").getAttribute('value');
-let b =document.getElementById("del1").getAttribute("value");
-    delete1.onclick = function(){
-        
-        if(a == b){
-        delEle();
-        }
-        a++;
-        b++
-    }
+
+//created delete function for deleting alarm
+
+let delButton = document.getElementsByClassName("delButton");
+let display =  document.getElementById("set-alarm-container");
+function delDiv(){
+    for(let i=0; i<delButton.length; i++){
+        delButton[i].onclick= function(){
+            for(let j=0; j<saveTime.length; j++){
+                if(display.childNodes[i].childNodes[0].innerHTML === saveTime[j]){
+                    
+                    saveTime.splice(j,1);
+                    
+                }
+            }
+            display.removeChild(display.childNodes[i]);
+            
+        }   }
 }
 
+setInterval(delDiv, 100);
 
 
-
-
-
+//reset function to reset my alarm  and stop playing sound
 function reset(){
     if (!sound.paused) { sound.pause(); }
       alarm = null;
@@ -155,7 +158,10 @@ function reset(){
 var btn = document.getElementById('tset');
 var resetBtn= document.getElementById('treset');
 
+//clicking ont set
 btn.onclick = set;
+
+//clicking on reset
 resetBtn.onclick = reset;
 
 
